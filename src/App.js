68580ef3ad7cb2b4cc502e7b2c8a71b0a1d4c41f
Nijanshu <nijanshusingh@gmail.com>
2024-04-26 23:10:30 +0530

    Created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,262 @@
-import logo from './logo.svg';
+import logo from './logo.png';
+import vid from './vid.mp4'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+<h1 className='text-center font-serif font-bold text-5xl mt-6'>What to do with <img className="h-14 inline-block" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGvEVKTquf-9w4HTx9rc7rfRZ08UQ91iMbNnGsmNswcw&s" alt="logo"/>
+  Bounty Box?</h1>
+
+ <section class="text-gray-600 body-font overflow-hidden">
+  <div class="container px-5 py-8 mx-auto">
+    <div class="lg:w-4/5 mx-auto flex flex-wrap">
+    <video width="350"  autoPlay>
+  <source src={vid} type="video/mp4"/>
+  Your browser does not support HTML video.
+</video>
+      <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+        <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">Congratulations! OnePlus</h1>
+        <div class="mt-4 mb-4">
+        <p>Dear OnePlus team,</p>
+
+<p>Heartfelt congratulations on reaching this monumental milestone of 10 years! Your journey has been nothing short of remarkable, filled with bold strides, revolutionary products, and an unwavering dedication to your community.</p>
+
+<p>In a decade, you've transformed the smartphone industry, not just with cutting-edge technology but also with a unique approach that values user feedback and fosters a sense of belonging among your fans. Your commitment to delivering premium experiences at accessible prices has won hearts worldwide.</p>
+
+<p>As you celebrate this significant achievement, take pride in knowing that your innovations have not only enhanced our daily lives but have also inspired a generation of tech enthusiasts.</p>
+
+<p>May the next chapter of your journey be filled with even greater success, innovation, and moments of joy.</p>
+<br/>
+<p>Never Settle!</p>
+<br/>
+<p>I'm truly excited about OnePlus's Bounty Box reward program, a fantastic initiative as part of your 10-year celebration. I'm eagerly participating in your giveaway, and I'm thrilled to share what I'd do if I were fortunate enough to win this coveted Bounty Box. I hope that you like my idea!</p>
     </div>
+        </div>
+        
+    </div>
+  </div>
+</section>
+ 
+<section class="text-gray-600 body-font">
+  <div class="container px-5 py-8 mx-auto">
+    <div class="flex flex-wrap -m-4">
+      <div class="p-4 md:w-1/3">
+        <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+          <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://i.gadgets360cdn.com/large/oneplus_buds_3_oneplus_1703677781291.jpg" alt="img"/>
+          <div class="p-6">
+            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">OnePlus Earbuds 3</h1>
+            <p class="leading-relaxed mb-3">Gifting one pair to my mom and one to my dad, so that they can have clear conversations everytime, also arguments should be clear without any misunderstanding 🤫. And one pair will be for myself obviously</p>
+            <div class="flex items-center flex-wrap ">
+              <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+                <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path d="M5 12h14"></path>
+                  <path d="M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="p-4 md:w-1/3">
+        <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+          <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://forumstatic.oneplusmobile.com/opforum-gl/upload/image/app/thread/20221103/9967780935552714301/1191213755213021187/1191213755213021187.jpeg" alt="img"/>
+          <div class="p-6">
+            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Una Collectibles</h1>
+            <p class="leading-relaxed mb-3">
+            One for me, one for my brother and one for my girlfriend as she is cute just like Una collectibles😌              </p>
+            <div class="flex items-center flex-wrap">
+              <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+                <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path d="M5 12h14"></path>
+                  <path d="M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="p-4 md:w-1/3">
+        <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+          <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://forumstatic.oneplusmobile.com/opforum-gl/upload/image/front/thread/20220614/871/1088824794537263108/1088824794537263108.png"/>
+         <div class="p-6">
+            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">OnePlus Pens</h1>
+            <p class="leading-relaxed mb-3">
+            One for me, one for my girlfriend for getting handwritten love letters everyday 💌, and she also loves to make digital art🖌️              </p>
+            <div class="flex items-center flex-wrap ">
+              <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+                <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path d="M5 12h14"></path>
+                  <path d="M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="p-4 md:w-1/3">
+        <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+          <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://www.oneplus.com/content/dam/oasis/page/2024/global/product/almond/watch2-share.jpg" alt="img"/>
+          <div class="p-6">
+            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">OnePlus Watch 2</h1>
+            <p class="leading-relaxed mb-3">
+            One for me and one for my girlfriend to check if we have the same heart rate 😉 and always meet at the right time⌚.
+            </p>
+            <div class="flex items-center flex-wrap ">
+              <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+                <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path d="M5 12h14"></path>
+                  <path d="M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="p-4 md:w-1/3">
+        <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+          <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://static.toiimg.com/thumb/msid-101924825,width-1280,height-720,resizemode-4/101924825.jpg" alt="img"/>
+          <div class="p-6">
+            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">OnePlus Keyboard 81 Pro</h1>
+            <p class="leading-relaxed mb-3">
+            One for me and one for my brother as we both like gaming and these keyboard are perfect for gaming 🎮 and programming⌨️
+            </p>
+            <div class="flex items-center flex-wrap ">
+              <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+                <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path d="M5 12h14"></path>
+                  <path d="M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="p-4 md:w-1/3">
+        <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+          <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://forumstatic.oneplusmobile.com/opforum-gl/upload/image/front/thread/20220615/115301/1089263891760283657/1089263891760283657.jpg" alt="img"/>
+          <div class="p-6">
+            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Power Of Community Water Bottles</h1>
+            <p class="leading-relaxed mb-3"> One for me and one for my brother for being hydrated in this summer 🌞 
+</p>
+            <div class="flex items-center flex-wrap ">
+              <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+                <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path d="M5 12h14"></path>
+                  <path d="M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="p-4 md:w-1/3">
+        <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+          <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://forumstatic.oneplusmobile.com/opforum-gl/upload/image/front/thread/20230112/2434351522862714301/1242343653125914629/1242343653125914629.jpg?x-ocs-process=image/format,webp/resize,w_1584" alt="img"/>
+          <div class="p-6">
+            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Power Of Community Duffle Bag</h1>
+            <p class="leading-relaxed mb-3">
+            I'm planning to go gym but always become lazy, if I get this bag I promise to go gym regularly 💪
+            </p>
+                        <div class="flex items-center flex-wrap ">
+              <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+                <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path d="M5 12h14"></path>
+                  <path d="M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="p-4 md:w-1/3">
+        <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+          <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjG-jChmmIOF-fZHWKLDmVTJQw7sV7t2dn_f_WHRwVZCS6QXWzGHuSXUl1i-aG90a0D_FTzZBWvSNc1EI3FpVGgT_jmaf896rlpwhdApqM7b58wjvgy1SkprnFZwOB1dCrwJSEcVwbqQjXnXnVv8pitSN-X2ATfNRCN-vGGuGZSS9hjwHFQZXmLqJwbEB0/s800/oneplus-adventure-backpack.png" alt="img"/>
+          <div class="p-6">
+            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">OnePlus Backpack </h1>
+            <p class="leading-relaxed mb-3">
+            I will this for myself to show that I never settle in one place just like one plus 👊
+            </p>
+              <div class="flex items-center flex-wrap ">
+              <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+                <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path d="M5 12h14"></path>
+                  <path d="M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="p-4 md:w-1/3">
+        <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+          <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://oasis.opstatics.com/content/dam/oasis/page/xueying/images-top-kv.jpg" alt="img"/>
+          <div class="p-6">
+            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">One Plus Open</h1>
+            <p class="leading-relaxed mb-3">
+            Obviously this one is for me 😏, because I'm a multitasker and with OnePlus Open, I can effortlessly juggle multiple tasks 🤹, maximizing productivity📈 without missing a beat. 
+            </p>
+            <div class="flex items-center flex-wrap ">
+              <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+                <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path d="M5 12h14"></path>
+                  <path d="M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<footer class="text-gray-600 body-font">
+  <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+    Created by Nijanshu Singh
+    <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">For
+      <a href="https://oneplus.com" class="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">
+        <img src="https://1000logos.net/wp-content/uploads/2022/11/OnePlus-Logo.png" className='inline' width={100} alt="" />
+      </a>
+    </p>
+    <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+      <a class="text-gray-500">
+        <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+          <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+        </svg>
+      </a>
+      <a class="ml-3 text-gray-500">
+        <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+          <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+        </svg>
+      </a>
+      <a class="ml-3 text-gray-500">
+        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+          <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+          <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+        </svg>
+      </a>
+      <a class="ml-3 text-gray-500">
+        <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" class="w-5 h-5" viewBox="0 0 24 24">
+          <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
+          <circle cx="4" cy="4" r="2" stroke="none"></circle>
+        </svg>
+      </a>
+    </span>
+  </div>
+</footer>
+    </div>
+
+    
   );
 }
 
